55eeb47dd614560e301406e98e801861ed385506
sohanassa <snassa01@ucy.ac.cy>
2020-04-12 18:42:18 +0300

    try and catch blocks for input in class ReadFromUser

--- src/ReadFromUser.java
+++ src/ReadFromUser.java
@@ -1,41 +1,53 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class ReadFromUser {
 
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		boolean succesfulInput=false;
+		int h=0,w=0,people=0,time=0,timeSpace=0,maskPers=0,immunePers=0;
+		double movingP=0,infectingP=0,infSpaceP=0,spaceInfHuman=0;
+	   do {
+       try{
+	       System.out.println("COVID-19 SIMULATOR!");
+		   System.out.print("Give size of place (height width): ");
+		   h=in.nextInt();
+		   w=in.nextInt();
 		
-		System.out.println("COVID-19 SIMULATOR!");
-		System.out.print("Give size of place (height width): ");
-		int h=in.nextInt();
-		int w=in.nextInt();
+		   System.out.print("Give number of people: ");
+		   people = in.nextInt();
 		
-		System.out.print("Give number of people: ");
-		int people = in.nextInt();
+		   System.out.print("Give time of simmulation in minuts: ");
+		   time = in.nextInt();
 		
-		System.out.print("Give time of simmulation in minuts: ");
-		int time = in.nextInt();
+		   System.out.print("Give time for a space to be safe: ");
+		   timeSpace = in.nextInt();
 		
-		System.out.print("Give time for a space to be safe: ");
-		int timeSpace = in.nextInt();
+		   System.out.print("Give possibility of moving (between 0-1): ");
+		   movingP = in.nextDouble();
 		
-		System.out.print("Give possibility of moving (between 0-1): ");
-		double movingP = in.nextDouble();
+		   System.out.print("Give possibility of infecting another human (between 0-1): ");
+		   infectingP = in.nextDouble();
 		
-		System.out.print("Give possibility of infecting another human (between 0-1): ");
-		double infectingP = in.nextDouble();
+		   System.out.print("Give possibility of infecting a space (between 0-1): ");
+		   infSpaceP = in.nextDouble();
 		
-		System.out.print("Give possibility of infecting a space (between 0-1): ");
-		double infSpaceP = in.nextDouble();
+		   System.out.print("Give possibility of getting infected froma a space (between 0-1): ");
+		   spaceInfHuman = in.nextDouble();
 		
-		System.out.print("Give possibility of getting infected froma a space (between 0-1): ");
-		double spaceInfHuman = in.nextDouble();
+		   System.out.print("Give percentage of mask use (between 0-100): ");
+		   maskPers = in.nextInt();
 		
-		System.out.print("Give percentage of mask use (between 0-100): ");
-		int maskPers = in.nextInt();
-		
-		System.out.print("Give percentage of immune peope (between 0-100): ");
-		int immunePers = in.nextInt();
+		   System.out.print("Give percentage of immune peope (between 0-100): ");
+		   immunePers = in.nextInt();
+		   succesfulInput=true;
+        }
+        catch(InputMismatchException e) {
+	         System.out.println("Wrong input, try again!");
+	         }
+       
+		}while(!succesfulInput);
 		
 		Simulate s= new Simulate(maskPers,immunePers,infectingP,infSpaceP,spaceInfHuman,movingP,h,w,people,timeSpace,time);
 		s.runSimulation();
