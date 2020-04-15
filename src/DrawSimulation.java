a37570cc977a536a163f70407356e91cf6f34fe1
ZoePassiadou <zpassi01@ucy.ac.cy>
2020-04-15 22:22:32 +0300

    comments in DrawSimulation

--- src/DrawSimulation.java
+++ src/DrawSimulation.java
@@ -2,24 +2,32 @@ import edu.princeton.cs.introcs.StdDraw;
 
 import java.awt.Color;
 
+/**
+ * This class creates an object called DrawSimulation used for drawing the canvas
+ * @author Zoe Passiadou
+ *@author Sohaib Nassar
+ */
 public class DrawSimulation {
-	int length;
-	int width;
+	int length;          //represents the length of the grid
+	int width;          //represents the width of the grid
 	
-	public DrawSimulation(int l, int w) {
+	public DrawSimulation(int l, int w) { //constructor
 		length=l;
 		width=w;
 	}
 	
+	/**
+	 * This method draws the grid 
+	 */
 	public void DrawGrid() {
 		
-	    StdDraw.setXscale(-1,length+1);                               
+	    StdDraw.setXscale(-1,length+1);  //setting the scale                             
         StdDraw.setYscale(-1,width+1);
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.setPenRadius(0.2/(length*2));
-        if(width<length)
+        StdDraw.setPenColor(StdDraw.BLACK); //setting the colour black
+        StdDraw.setPenRadius(0.2/(length*2));  //set pen radius
+        if(width<length)                                //draw the scale
 		 for(int i=0;i<=this.length;i++) {                                
-  		     StdDraw.line(0.0,0.0+i,0.0+length,0.0+i);
+  		     StdDraw.line(0.0,0.0+i,0.0+length,0.0+i); //draw lines diagonally and horizontally
   		     StdDraw.line(0.0+i,0.0,0.0+i,0.0+width);
          }
         else
@@ -30,23 +38,42 @@ public class DrawSimulation {
             }
 	}
 
+/**
+ * This method Draws a human in given position
+ * @param x represents the row
+ * @param y represents the column
+ * @param c colour of the human
+ */
 public void DrawHuman(int x, int y, Color c) {
-	StdDraw.setPenColor(c);
-	StdDraw.filledCircle(x+0.5, y+0.5, 0.4);
+	StdDraw.setPenColor(c); //set the pen to given colour
+	StdDraw.filledCircle(x+0.5, y+0.5, 0.4); // draw circle in give position
 }
 	
+/**
+ * This method Draws infected areas red
+ * @param i represents the row
+ * @param j represents the column
+ */
 public void DrawInfectedArea(int i, int j){
 	
 	StdDraw.setPenColor(StdDraw.RED);
-	StdDraw.filledRectangle(i+0.5, j+0.5, 0.4, 0.4); //idk what this is gonna look like damn
+	StdDraw.filledRectangle(i+0.5, j+0.5, 0.4, 0.4); 
 	
 }
+/**
+ * This method Draws disinfects area
+ * @param i represents the row
+ * @param j represents the column
+ */
 public void DisInfectArea(int i,int j) {
 	
 	StdDraw.setPenColor(StdDraw.WHITE);
 	StdDraw.filledRectangle(i+0.5, j+0.5, 0.4, 0.4);
 }
 
+/**
+ * resets the canvas
+ */
 public void FillAll() {
 	StdDraw.filledRectangle(0, 0, length, width);
 	DrawGrid();
